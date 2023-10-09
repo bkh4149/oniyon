3c1e5082954e705da1d5c6c370e6b4075db0a6f7
bkh4149 <bkh4149@gmail.com>
2023-10-09 11:41:09 +0900

    複数の問題に対応できるようにした

--- server.py
+++ server.py
@@ -8,14 +8,25 @@ app.config['SECRET_KEY'] = 'your_secret_key_here'
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
+sets = [
+    ["問題1 今月は何月ですか？", "6月:7月:8月:9月:10月:11月:12月", "10月", "説明1"],
+    ["問題2 以下の動物の中で鳥はどれ？", "ライオン:象:ペンギン:カンガルー:カモメ:スズメ", "ペンギン:カモメ:スズメ", "ペンギンは鳥の一種ですが、飛べません。"],
+    ["問題3 最も大きな惑星は？", "地球:火星:木星:金星", "木星", "木星は太陽系で最も大きな惑星です。"],
+    ["問題4 日本の首都は？", "大阪:東京:福岡:仙台", "東京", "日本の首都は東京です。"],
+    ["問題5 以下の中で果物はどれ？", "ピーマン:キャベツ:ブロッコリー:メロン:パイナップル:バナナ", "メロン:パイナップル:バナナ", "メロン:パイナップル:バナナは果物の一種ですが、野菜としても扱われることが多いです。"],
+    ["問題6 以下の言語の中でスペイン語で「こんにちは」は？", "Hello:Bonjour:Halo:Hola", "Hola", "スペイン語で「こんにちは」は「Hola」と言います。"],
+]
+
+
 @app.route('/')
 def home():
+    session["q_no"] = 2
     return redirect('/question', code=302)
-
 
 @app.route('/question') #questionが飛んできたらプログラムが実行
 def q1():
-    q1= ["問題1 今月は何月ですか？", "6月:7月:8月:9月:10月:11月:12月", "10月:11月", "説明1"]
+    q_no=session["q_no"]
+    q1= sets[q_no]
     print(q1[0])  # 質問文の表示
 
     arr = q1[1].split(":")  # 解答群の作成　多数の中から４つをランダムで選択
@@ -34,7 +45,7 @@ def q1():
     correct_choices = set(result) & cs_temp
     session["correct_ans"] = correct_choices
 
-    return render_template('index.html', question="問題1 今月は何月ですか？", choices=result)
+    return render_template('index.html', question=q1[0], choices=result)
 
 @app.route('/answer', methods=['GET']) #answerが飛んできたら下のプログラムが実行
 def check_answer():
